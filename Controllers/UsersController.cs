f4edbe58a6df0f0819a99a4dc463d9fc9eec4cfa
Leonard Botha <leonard.lb341@gmail.com>
2021-01-05 20:23:41 +0200

    add json response for users endpoint

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -17,45 +17,20 @@ namespace ProactiveBot.Controllers
     public class UsersController : ControllerBase
     {
         private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferences;
-        private ClientCredentialProvider _authProvider;
         public UsersController(ConcurrentDictionary<string, ConversationReference> conversationReferences,
             IConfiguration configuration)
         {
             _conversationReferences = conversationReferences;
-
-            //IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
-            //.Create(configuration["MicrosoftAppId"])
-            //.WithTenantId(configuration["TenantID"])
-            //.WithClientSecret(configuration["MicrosoftAppPassword"])
-            //.Build();
-
-            //_authProvider = new ClientCredentialProvider(confidentialClientApplication);
         }
 
 
             public async Task<IActionResult> Get()
         {
-            StringBuilder content = new StringBuilder();
-            content.Append("<html><body><h1>USERS</h1>");
-            foreach (var conversationReference in _conversationReferences)
-            {
-                content.Append("<h3>USERID: " + conversationReference.Value.User.Id.Substring(0,7)+"</h3>");
-                content.Append("<h3>USERNAME: "+conversationReference.Value.User.Name == null ||
-                    conversationReference.Value.User.Name  == string.Empty? "NoName":
-                    conversationReference.Value.User.Name + "</h3>");
-                content.Append("<hr><br>");
-            }
-            if (_conversationReferences.Count == 0)
-            {
-                content.Append("<h1>No Conversation refs</h1>");
-            }
-            content.Append("</body></html>");
-
             // Let the caller know proactive messages have been sent
             return new ContentResult()
             {
-                Content = content.ToString(),
-                ContentType = "text/html",
+                Content = JsonSerializer.Serialize(_conversationReferences),
+                ContentType = "application/json",
                 StatusCode = (int)HttpStatusCode.OK,
             };
         }
