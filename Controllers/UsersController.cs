3b87992bfd22b9488c17ee726ca720d022b8067a
Len341 <50423260+Len341@users.noreply.github.com>
2021-02-24 09:31:43 +0200

    Add password to view userslist

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -28,51 +28,64 @@ namespace ProactiveBot.Controllers
 
         public async Task<IActionResult> Get() 
         {
-            List<User> users = new List<User>();
-
-            var confidentialClient = ConfidentialClientApplicationBuilder
-                .Create("1aafdd77-ada0-47ad-bc7a-ce6e59cdb7b9")
-                .WithAuthority($"https://login.microsoftonline.com/"+ "c31a1761-7d00-4b39-91a5-cc79c01bc9f6" + "/v2.0")
-                .WithClientSecret("4-L6d8x.RYACAq2X.6xwdgzIu_NQm709uU")
-                .Build();
-            var scopes = new string[] { "https://graph.microsoft.com/.default" };
-            GraphServiceClient graphServiceClient = null;
-            Microsoft.Graph.User me = null;
-            graphServiceClient =
-                new GraphServiceClient(new DelegateAuthenticationProvider(async (requestMessage) => {
-                    // Retrieve an access token for Microsoft Graph (gets a fresh token if needed).
-                    var authResult = await confidentialClient
-                        .AcquireTokenForClient(scopes)
-                        .ExecuteAsync();
-
-                    // Add the access token in the Authorization header of the API request.
-                    requestMessage.Headers.Authorization =
-                        new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
-
-                     me = await graphServiceClient.Me
-                    .Request()
-                    .GetAsync();
-                })
-            );
-
-            System.Console.WriteLine(me);
-            foreach (var reference in _conversationReferences)
+            if (Request.Query["pass"] == "ourpassword")
             {
-                users.Add(new User()
+                List<User> users = new List<User>();
+
+                //var confidentialClient = ConfidentialClientApplicationBuilder
+                //    .Create("1aafdd77-ada0-47ad-bc7a-ce6e59cdb7b9")
+                //    .WithAuthority($"https://login.microsoftonline.com/"+ "c31a1761-7d00-4b39-91a5-cc79c01bc9f6" + "/v2.0")
+                //    .WithClientSecret("4-L6d8x.RYACAq2X.6xwdgzIu_NQm709uU")
+                //    .Build();
+                //var scopes = new string[] { "https://graph.microsoft.com/.default" };
+                //GraphServiceClient graphServiceClient = null;
+                //Microsoft.Graph.User me = null;
+                //graphServiceClient =
+                //    new GraphServiceClient(new DelegateAuthenticationProvider(async (requestMessage) => {
+                //        // Retrieve an access token for Microsoft Graph (gets a fresh token if needed).
+                //        var authResult = await confidentialClient
+                //            .AcquireTokenForClient(scopes)
+                //            .ExecuteAsync();
+
+                //        // Add the access token in the Authorization header of the API request.
+                //        requestMessage.Headers.Authorization =
+                //            new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+                //         me = await graphServiceClient.Me
+                //        .Request()
+                //        .GetAsync();
+                //    })
+                //);
+                //System.Console.WriteLine(me);
+
+
+                foreach (var reference in _conversationReferences)
                 {
-                    Name = reference.Value.User.Name,
-                    ID = reference.Value.User.Id,
-                    Email = reference.Value.User.Properties.Value<string>("Email"),
-                    OrgID = reference.Value.User.Properties.Value<string>("OrgID")
-                });
-                //testcommit
+                    users.Add(new User()
+                    {
+                        Name = reference.Value.User.Name,
+                        ID = reference.Value.User.Id,
+                        Email = reference.Value.User.Properties.Value<string>("Email"),
+                        OrgID = reference.Value.User.Properties.Value<string>("OrgID")
+                    });
+                    //testcommit
+                }
+                return new ContentResult()
+                {
+                    Content = JsonSerializer.Serialize(users),
+                    ContentType = "application/json",
+                    StatusCode = (int)HttpStatusCode.OK,
+                };
             }
-            return new ContentResult()
+            else
             {
-                Content = JsonSerializer.Serialize(users),
-                ContentType = "application/json",
-                StatusCode = (int)HttpStatusCode.OK,
-            };
+                return new ContentResult()
+                {
+                    Content = "{'error': 'unauthorized'}",
+                    ContentType = "application/json",
+                    StatusCode = (int)HttpStatusCode.Forbidden,
+                };
+            }
         }
     }
 
